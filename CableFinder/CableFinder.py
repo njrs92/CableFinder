5113d574d9a938c69c636c8153d4bdf66907b508
Nicholas Stevens <Stevens.Nicholas@abc.net.au>
2018-06-28 23:16:28 +1000

    Pickeling Now works

--- CableFinder/CableFinder.py
+++ CableFinder/CableFinder.py
@@ -1,7 +1,8 @@
 import os
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import *
 import pandas as pd
+import pickle
 
 file_path = ""
 
@@ -11,42 +12,64 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "load Spreadsheet"
-        self.hi_there["command"] = self.result
-        self.hi_there.pack(side="top")
+    def create_widgets(self):    
+        self.readSpreadSheet = tk.Button(self)
+        self.readSpreadSheet["text"] = "Load Cable Data Spreadsheet"
+        self.readSpreadSheet["command"] = self.loadspreadsheet
+        self.readSpreadSheet.pack(side="top")
 
+        self.readPickel =tk.Button(self)
+        self.readPickel["text"] = "Load Pickel"
+        self.readPickel["command"] = self.pickeled
+        self.readPickel.pack()
         self.quit = tk.Button(self, text="QUIT", fg="red",command=root.destroy)
         self.quit.pack(side="bottom")
 
-    def result(self):
+    def loadspreadsheet(self):
         root.withdraw()
-        result = tk.Toplevel(self)
-        result.geometry('500x500')
-
-        data_state = tk.Label(result, text="Loading Cable Data")
+        loadspreadsheet = tk.Toplevel(self)
+        data_state = tk.Label(loadspreadsheet, text="Loading Cable Data")
         data_state.pack()
         file_path = askopenfilename()
-        xl = pd.ExcelFile(file_path)
-        data_state.configure(text = "Data Loaded")
-        tk.Label(result, text="Please Select Type of Cable").pack()
+        xl = pd.read_excel(file_path,sheet_name=None)                
+        data_state.configure(text = "Data Loaded")                
+        tk.Label(loadspreadsheet, text="Please Choose Where to Save the Pickel").pack()        
         data_state.update()
-        Sheet_choice = tk.StringVar()
-        for x in xl.sheet_names:
-            tk.Radiobutton(result, text=x, variable=Sheet_choice, value=x).pack()       
-        tk.Label(result, text="Enter Cable Number").pack()
-        cable_number = tk.Entry(result).pack()
-        tk.Button(result, text="Find",command=lambda: self.extract(xl,Sheet_choice)).pack()        
-        self.quit = tk.Button(result, text="QUIT", fg="red",command=root.destroy)
-        self.quit.pack(side="bottom")
-    
-    def extract(self,xl,Sheet_choice):
-        #df1 = xl.parse(Sheet_choice)
-        extract = tk.Toplevel(self)
-        tk.Label(extract, text="Sheet Selcted").pack()
-        tk.Label(extract, text=Sheet_choice.get()).pack()       
+        file_path = askopenfilename()
+        f = open(file_path,'wb')
+        pickle.dump(xl,f)
+        f.close()
+        root.deiconify()
+        loadspreadsheet.destroy()
         
+    def pickeled(self):
+        root.withdraw()
+        pickeled = tk.Toplevel(self)
+        pickle_state = tk.Label(pickeled, text="Please Pick The pickle")
+        pickle_state.pack()
+        file_path = askopenfilename()
+        ws = pickle.load(open(file_path,'rb'))
+        pickle_state.configure(text = "Data Loaded")
+        pickle_state.update()
+        self.findcable(ws)
+        pickeled.destroy()
+
+    def findcable(self,ws):
+        findcable = tk.Toplevel(self)
+        pickle_state = tk.Label(findcable, text="Please Choose the type of Cable")
+        pickle_state.pack()
+        sheets= tk.StringVar()
+        for x in ws:
+            tk.Radiobutton(findcable, text=x,variable=sheets,value=x).pack() 
+        e = Entry(findcable)
+        e.pack()
+        tk.Button(findcable, text="Find Cable",command=lambda: self.results(ws,e.get())).pack()       
+        tk.Button(self, text="QUIT", fg="red",command=root.destroy).pack()
+
+
+
+    def results(self,ws,cable):
+        results = tk.Toplevel(self)
 
 root = tk.Tk()
 app = Application(master=root)
